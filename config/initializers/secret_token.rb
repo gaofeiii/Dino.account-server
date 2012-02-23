176aad5f4c1866cdf096d54d8debd24ab25cffc5
gaofei <Apple@ApplematoMac-mini.local>
2012-02-23 17:55:17 +0800

    Init project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accounts::Application.config.secret_token = '0f539283d2b71a64807e4b4cf161b751616b679797ed648bf3604f30c89576e75233202b6b40e1888bb4b592012811fae2bdfd701e2c4ad282bd3004c66e4761'
