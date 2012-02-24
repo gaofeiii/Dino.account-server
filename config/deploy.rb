3e72c5dcf9e067263a792dfb0afb3d88793914c0
gaofei <Apple@ApplematoMac-mini.local>
2012-02-24 13:45:36 +0800

    add unicorn configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,35 @@
-set :application, "accounts"
-set :repository,  "git@96.126.121.100:/git/accounts.git"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
 
+set :rvm_ruby_string, "1.9.3@rails321"
+set :rvm_type, :user
+
+set :application, "accounts"
+set :user, "xiaojuhua"
+set :password, "really"
+set :runner, "xiaojuhua"
+set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :keep_releases, 5
+
+set :repository,  "git@96.126.121.100:/git/accounts.git"
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "master"
 
 role :web, "96.126.121.100"                        # Your HTTP server, Apache/etc
 role :app, "96.126.121.100"                          # This may be the same as your `Web` server
 role :db,  "96.126.121.100", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :unicorn do
+  desc "start unicorn"
+  task :start do
+    run "unicorn -c #{current_path}/config/unicorn.rb -E production -D"
+  end
+  
+  desc "stop unicorn"
+  task :stop do
+    run "kill -Q cat `#{shared_path}/pids/unicorn.pid`"
+  end
+end
