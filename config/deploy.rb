f64c7986b7789342c7eef3251782a828d0c5b933
gaofei <Apple@ApplematoMac-mini.local>
2012-02-24 13:11:54 +0800

    change production database

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "accounts"
+set :repository,  "git@96.126.121.100:/git/accounts.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "96.126.121.100"                        # Your HTTP server, Apache/etc
+role :app, "96.126.121.100"                          # This may be the same as your `Web` server
+role :db,  "96.126.121.100", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
