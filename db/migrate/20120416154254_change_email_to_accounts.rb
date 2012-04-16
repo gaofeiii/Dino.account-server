d2b0ca730c5021116955a6888cde512539d3285b
magic0fei <magic0fei@gmail.com>
2012-04-17 00:00:05 +0800

    fix migration file error.

--- db/migrate/20120416154254_change_email_to_accounts.rb
+++ db/migrate/20120416154254_change_email_to_accounts.rb
@@ -1,7 +1,7 @@
 class ChangeEmailToAccounts < ActiveRecord::Migration
   def change
   	change_table :accounts do |t|
-  		t.remove :accounts, :email
+  		t.remove :email
   		t.string :email
   		t.index :email, :unique => true
   	end
